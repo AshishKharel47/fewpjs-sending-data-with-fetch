dc6347e28827d3e32ffa76c468d5850191badc0c
AshishKharel <ashishkharel58@gmail.com>
2021-04-11 22:22:38 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+function submitData(name,email){
+    return fetch('http://localhost:3000/users',{
+        method: "POST",
+        headers: {
+            "content-Type":"application/json",
+            "Accept":"application/json"
+        },
+        body: JSON.stringify({
+            name,
+            email
+        })
+    })
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(object){
+        document.body.innerHTML=object["id"]
+    })
+    .catch(function(Error){
+    document.body.innerHTML = Error.message
+    })
+}
